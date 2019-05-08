14e7991cdf95e0dd01d2b3ecf2a00e9f71ae72c0
zhang-xiao-miao <1286874200@qq.com>
2019-05-08 19:37:21 -0400

    mv code folder to root path

--- design-pattern/mvc/sample.js
+++ design-pattern/mvc/sample.js
@@ -2,6 +2,7 @@ import Student from './Student';
 import StudentController from './StudentController';
 import StudentView from './StudentView';
 
+// system
 function showSample() {
   const model = new Student('Kathy');
   const view = new StudentView();
